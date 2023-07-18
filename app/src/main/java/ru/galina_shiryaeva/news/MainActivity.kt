ee9b7a9316f14afc990c3bbeb54e691546ad051d
GalinaShiryaeva <Galla1777.35@gmail.com>
2023-07-18 10:38:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/galina_shiryaeva/news/MainActivity.kt
@@ -0,0 +1,26 @@
+package ru.galina_shiryaeva.news
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import ru.galina_shiryaeva.news.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
